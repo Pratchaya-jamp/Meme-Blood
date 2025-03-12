3d4c44b2a9168d4e3e7c39d846f47f4e6570e0ec
SongkiatchaiWongthaidee <142302402+SongkiatchaiWongthaidee@users.noreply.github.com>
2025-03-12 10:54:01 +0700

    035-[Add] Layout
    - component Hand
    - component GameManager

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
+import GameManager from './components/GameManager.vue'
 
 </script>
 
 <template>
-
+  <GameManager />
 </template>
 
 <style scoped>
--- src/components/GameManager.vue
+++ src/components/GameManager.vue
@@ -1,4 +1,33 @@
-<script>
-//main game contianer boardเล่นเกมนั้นละ
-//ประกอบด้วยlane hand deck card square
+<script setup>
+import Hand from "./mainGameComponents/Hand.vue";
+
+// Everytime card in put on table must update data in table -> for card operation (use computed)
 </script>
+
+<template>
+  <div class="flex items-center justify-center h-screen w-screen gap-20 -my-25 px-20">
+    <!-- Left Section (Image & Name) -->
+    <div class="flex flex-col items-center mr-auto">
+      <img src="../assets/logo.svg" alt="Left Player" class="w-20 h-20" />
+      <p class="mt-2 font-bold">Left Player</p>
+    </div>
+
+    <!-- Table -->
+    <table class="border border-gray-800">
+      <tbody>
+        <tr v-for="row in 3" :key="row">
+          <td v-for="col in 7" :key="col" class="border border-gray-600 w-30 h-40 text-center">
+            Row {{ row }} <br> Col {{ col }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+
+    <!-- Right Section (Image & Name) -->
+    <div class="flex flex-col items-center ml-auto">
+      <img src="../assets/logo.svg" alt="Right Player" class="w-20 h-20" />
+      <p class="mt-2 font-bold">Right Player</p>
+    </div>
+  </div>
+  <Hand />
+</template>
