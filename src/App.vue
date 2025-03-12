76143fd4ef50abf5334f11cec8d28872877b5a0d
Natkamon Chotipiboonsub <natkamon.chot@kmutt.ac.th>
2025-03-12 18:38:35 +0700

    028 - [add] Random Head or Tail system
    - Create new component named HeadOrTail.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import GameManager from './components/GameManager.vue'
+import HeadOrTail from './components/mainGameComponents/HeadOrTail.vue';
 import PlayerManager from './components/PlayerManager.vue';
 
 </script>
 
 <template>
-  <!-- <GameManager />-->
-   <PlayerManager/>
+  <!-- <GameManager /> -->
+   <!-- <PlayerManager/> -->
+    <HeadOrTail />
 </template>
 
 <style scoped>
