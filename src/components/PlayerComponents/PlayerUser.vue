7ce994f7a30c1eec0ca377eb671a463f0a147e3b
Natkamon Chotipiboonsub <natkamon.chot@kmutt.ac.th>
2025-03-15 20:59:58 +0700

    028 - [update] pull code from branch main
    JSON update

--- src/components/PlayerComponents/PlayerUser.vue
+++ src/components/PlayerComponents/PlayerUser.vue
@@ -16,11 +16,6 @@ const userprop = defineProps({
 </script>
 
 <template>
-    <div class="player-user text-white">
-        <p>Welcome, <span class="font-semibold">{{ userprop.users.username }}</span>!</p>
-        <p class="text-sm text-gray-400">User ID: {{ userprop.users.uid }}</p>
-    </div>
-
 </template>
 
 <style scoped></style>
