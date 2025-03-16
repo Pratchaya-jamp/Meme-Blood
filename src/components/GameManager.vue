0f65d0d3a19fff220e21f7fe996915fdad049cdd
Natkamon Chotipiboonsub <natkamon.chot@kmutt.ac.th>
2025-03-16 18:07:15 +0700

    028 - [add] player character in game
    - Profile character in game
    - Update PlayerCharacter component
    - Add player character into GameManager component

--- src/components/GameManager.vue
+++ src/components/GameManager.vue
@@ -1,11 +1,28 @@
 <script setup>
 import Hand from "./mainGameComponents/Hand.vue";
 import TableGame from "./mainGameComponents/Table.vue";
+import PlayerCharacter from "./mainGameComponents/PlayerCharacter.vue";
 
 // Everytime card in put on table must update data in table -> for card operation (use computed)
 </script>
 
 <template>
-  <TableGame />
+  <div class="flex items-center justify-center gap-20">
+    <!-- Left Player -->
+    <PlayerCharacter selectId="1669">
+      <template #image>
+        <img src="../assets/logo.svg" alt="Left Player" class="w-24 h-24 rounded-full shadow-lg">  
+      </template>
+    </PlayerCharacter>
+
+    <TableGame />
+    
+    <!-- Right Player -->
+    <PlayerCharacter selectId="9f71">
+      <template #image>
+        <img src="../assets/logo.svg" alt="Right Player" class="w-24 h-24 rounded-full shadow-lg">  
+      </template>
+    </PlayerCharacter>
+  </div>
   <Hand />
 </template>
--- src/components/mainGameComponents/PlayerCharacter.vue
+++ src/components/mainGameComponents/PlayerCharacter.vue
@@ -1,3 +1,25 @@
-<script>
+<script setup>
 //เเสดงตัวที่playerเลือกเล่น
+import { onMounted, ref } from 'vue';
+const props = defineProps(['selectId']);
+const characters = ref(null);
+
+onMounted(async () => {
+    const response = await fetch('data/db.json');
+    const data = await response.json();
+    characters.value = data.character.find(cha => cha.id === props.selectId);
+})
 </script>
+
+<template>
+    <div v-if="characters" class="character-card">
+        <slot name="name">{{ characters.charatername }}</slot>
+        <slot name="image">
+            <img :src="`/images/${characters.charatername.toLowerCase()}.png`" :alt="characters.charatername">
+        </slot>
+        <p>Skill: {{ characters.skill }}</p>
+    </div>
+    <div v-else>
+        Loading character...
+    </div>
+</template>
