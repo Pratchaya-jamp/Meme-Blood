3f82dad99e00186ad70571b53a0c8f4682bfd726
SongkiatchaiWongthaidee <142302402+SongkiatchaiWongthaidee@users.noreply.github.com>
2025-03-19 15:17:59 +0700

    035-[add] from branch main

--- src/components/PlayerComponents/PlayerInventory.vue
+++ src/components/PlayerComponents/PlayerInventory.vue
@@ -21,8 +21,9 @@ const inventoryProp = defineProps({
     }
 });
 const selectedDeck = ref()
-const isEditingDeck = ref(false)
 const selectedInventoryCards = ref([]);
+const addCard = ref(false)
+const removeCard = ref(false)
 
 const inventoryDetails = computed(() => {
     return inventoryProp.inventory.map(item => ({
@@ -50,10 +51,11 @@ const getCardsInDeck = computed(() => {
 
 const getCardsInInventory = computed(() =>{
     const cardsInInventory = inventoryProp.cards.filter(card => inventoryDetails.value.some(inv => inv.card.includes(card.idcard)))
-
-    if(cardsInInventory){
+    if(!selectedDeck.value || !getCardsInDeck.value){
         return cardsInInventory
     }
+    const cardInDeck = getCardsInDeck.value.map(card => card && card.idcard).filter(id => id !== undefined)
+    return cardsInInventory.filter(card => !cardInDeck.includes(card.idcard))
 })
 const editingDeck = async () =>{
     if(!selectedDeck.value || selectedInventoryCards.value.length === 0){
@@ -62,79 +64,141 @@ const editingDeck = async () =>{
     }
     else{
         let deckToEdit = inventoryProp.decks.find(deck => deck.deckid === selectedDeck.value)
-        selectedInventoryCards.value.forEach(card => {
+        if(addCard.value){
+            selectedInventoryCards.value.forEach(card => {
             if(!deckToEdit.cardid.includes(card.idcard)){
                 deckToEdit.cardid.push(card.idcard)
-            }
-        })
-        try{
-            const editedDeck = await editItem(`${import.meta.env.VITE_APP_URL}/deck`, deckToEdit.id, deckToEdit);
+            }})
+            try{
+            const editedDeck = await editItem(`${import.meta.env.VITE_APP_URL}/deck`, deckToEdit.id, deckToEdit)
             if (editedDeck) {
                 alert('Deck updated successfully')
+                selectedInventoryCards.value = []
             }
-        }catch (error) {
-            console.error('Failed to update deck:', error)
+            }catch (error) {
+                console.error('Failed to update deck:', error)
+            }
+        }
+        if(removeCard.value){
+            deckToEdit.cardid = deckToEdit.cardid.filter(
+                cardId => !selectedInventoryCards.value.some(card => card.idcard === cardId)
+            )
+            try{
+                const editedDeck = await editItem(`${import.meta.env.VITE_APP_URL}/deck`, deckToEdit.id, deckToEdit)
+                if(editedDeck){
+                    alert('Card(s) removed from deck successfully')
+                    selectedInventoryCards.value = []
+                    setNormalState()
+                }
+                }catch(error){
+                    console.error('Failed to remove cards from deck:', error)
+                }
+            }
         }
     }
+
+const setAddCard = () =>{
+    addCard.value = true
+    removeCard.value = false
+}
+
+const setRemoveCard = () =>{
+    removeCard.value = true
+    addCard.value = false
+}
+const setNormalState = () =>{
+    addCard.value = false
+    removeCard.value = false
 }
 const selectInventoryCardFunc = (card) => {
-    const index = selectedInventoryCards.value.findIndex(selectedCard => selectedCard.idcard === card.idcard);
-    if (index === -1) {
-        selectedInventoryCards.value.push(card);
-    } else {
-        selectedInventoryCards.value.splice(index, 1);
+    const index = selectedInventoryCards.value.findIndex(selectedCard => selectedCard.idcard === card.idcard)
+
+    if(removeCard.value){
+        const isInDeck = getCardsInDeck.value.some(deckCard => deckCard && deckCard.idcard === card.idcard)
+        if(isInDeck){
+            if(index === -1){
+                selectedInventoryCards.value.push(card)
+            } else {
+                selectedInventoryCards.value.splice(index, 1)
+            }
+        
+        } else{
+            alert('Please select a card from the deck to remove.')
+            return
+        }
+    } 
+    else if(addCard.value){
+        const isInInventory = getCardsInInventory.value.some(invCard => invCard.idcard === card.idcard)
+        if(isInInventory){
+            if(index === -1){
+                selectedInventoryCards.value.push(card)
+            } else{
+                selectedInventoryCards.value.splice(index,1)
+            }
+        } else {
+            alert('Please select a card from the inventory to add.')
+            return
+        }
     }
 }
 </script>
 
 <template>
     <div class="inventory-container bg-gray-800 p-6 rounded-lg shadow-lg max-w-full flex-grow">
-        <h2 class="text-2xl font-semibold mb-4 text-center text-white">Player Inventory</h2>
-
-        <div v-if="inventoryDetails.length > 0">
-            <h3 class="text-lg font-semibold text-white mb-2">Inventory Details:</h3>
-
-            <label for="selectedDeck" class="block text-gray-200 text-sm font-bold mb-2 w-fit">Select Deck:</label>
-            <select v-model="selectedDeck" id="selectedDeck" class="shadow border rounded w-full py-2 px-3 bg-gray-700 text-white border-gray-600">
-                <option v-for="deck in uniqueDecks" :key="deck" :value="deck">{{ deck }}</option>
-            </select>
-
-            <div v-if="selectedDeck && getCardsInDeck && getCardsInDeck.length > 0" class="mt-4 flex flex-wrap gap-4">
-                <h4 class="text-lg font-semibold text-white mb-2 w-full">Cards in Selected Deck:</h4>
-                <div v-for="card in getCardsInDeck" :key="card.idcard" @click="selectInventoryCardFunc(card)"
-                     :class="{ 'border-blue-500 border-4': selectedInventoryCards.some(selectedCard => selectedCard.idcard === card.idcard) }"
-                     class="cursor-pointer relative w-36 h-48 bg-gray-800 border-4 border-gray-600 rounded-lg shadow-lg hover:scale-105 transition-transform">
-                    <div class="absolute top-2 left-2 bg-yellow-500 text-white text-sm px-2 py-1 rounded">
-                        ID: {{ card.idcard }}</div>
-                    <div class="absolute bottom-2 w-full text-center text-sm text-yellow-300 font-semibold">{{ card.cardname }}</div>
-                </div>
-            </div>
-
-            <button @click="editingDeck" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-4">Add Card to Deck</button>
+      <h2 class="text-2xl font-semibold mb-4 text-center text-white">Player Inventory</h2>
+  
+      <div v-if="inventoryDetails.length > 0">
+        <h3 class="text-lg font-semibold text-white mb-2">Inventory Details:</h3>
+  
+        <label for="selectedDeck" class="block text-gray-200 text-sm font-bold mb-2 w-fit">Select Deck:</label>
+        <select v-model="selectedDeck" id="selectedDeck" class="shadow border rounded w-full py-2 px-3 bg-gray-700 text-white border-gray-600">
+          <option v-for="deck in uniqueDecks" :key="deck" :value="deck">{{ deck }}</option>
+        </select>
+  
+        <div v-if="selectedDeck && getCardsInDeck && getCardsInDeck.length > 0" class="mt-4 flex flex-wrap gap-4">
+          <h4 class="text-lg font-semibold text-white mb-2 w-full">Cards in Selected Deck:</h4>
+          <p v-if="removeCard">Remove Card From Deck</p>
+          <div v-for="card in getCardsInDeck" :key="card.idcard"
+              @click="selectInventoryCardFunc(card)"
+              :class="[ 'cursor-pointer relative w-36 h-48 bg-gray-800 border-4 border-gray-600 rounded-lg hover:scale-105 transition-transform',
+               selectedInventoryCards.some(selectedCard => selectedCard.idcard === card.idcard) ? 'shadow-lg' : '', 
+               addCard && selectedInventoryCards.some(selectedCard => selectedCard.idcard === card.idcard) ? 'bg-green-600' : '', 
+               removeCard && selectedInventoryCards.some(selectedCard => selectedCard.idcard === card.idcard) ? 'bg-red-600' : '']">
+            <div class="absolute top-2 left-2 bg-yellow-500 text-white text-sm px-2 py-1 rounded">
+              ID: {{ card.idcard }}</div>
+            <div class="absolute bottom-2 w-full text-center text-sm text-yellow-300 font-semibold">{{ card.cardname }}</div>
+          </div>
         </div>
-        <ul class="text-white">
-            <li v-for="item in inventoryDetails" :key="item.deckid">
-                <div class="border-b border-gray-600 pb-2 mb-2">
-                    <p>Inventory ID: {{ item.deckid }}</p>
-                    <h4 class="text-lg font-semibold text-white mb-2 w-full">Cards in Inventory</h4>
-                    <div class="flex flex-wrap gap-x-4 gap-y-4">
-                        <div v-for="card in getCardsInInventory" :key="card.idcard" 
-                            @click="selectInventoryCardFunc(card)"
-                            :class="{ 'border-blue-500 border-4': selectedInventoryCards.some(selectedCard => selectedCard.idcard === card.idcard) }"
-                            class="cursor-pointer relative w-36 h-48 bg-gray-800 border-4 border-gray-600 rounded-lg shadow-lg hover:scale-105 transition-transform">
-                            <div class="absolute top-2 left-2 bg-yellow-500 text-white text-sm px-2 py-1 rounded">
-                                ID: {{ card.idcard }}
-                            </div>
-                            <div class="absolute bottom-2 w-full text-center text-sm text-yellow-300 font-semibold">
-                                {{ card.cardname }}
-                            </div>
-                        </div>
-                    </div>
-                    <p>Character: <span v-for="char in item.character" :key="char">(ID: {{ char }})</span></p>
+  
+        <button @click="setAddCard" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-4">Add Card to Deck</button>
+        <button @click="setRemoveCard" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-4">Remove Card from Deck</button>
+        <button @click="setNormalState" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-4">Cancel</button>
+      </div>
+      <ul class="text-white">
+        <li v-for="item in inventoryDetails" :key="item.deckid">
+          <div class="border-b border-gray-600 pb-2 mb-2">
+            <h4 class="text-lg font-semibold text-white mb-2 w-full">Cards in Inventory</h4>
+            <p v-if="addCard">Add Card To Deck</p>
+            <div class="flex flex-wrap gap-x-4 gap-y-4">
+              <div v-for="card in getCardsInInventory" :key="card.idcard"
+                   @click="selectInventoryCardFunc(card)"
+                   :class="[ 'cursor-pointer relative w-36 h-48 bg-gray-800 border-4 border-gray-600 rounded-lg hover:scale-105 transition-transform',
+                   selectedInventoryCards.some(selectedCard => selectedCard.idcard === card.idcard) ? 'shadow-lg' : '',
+                   addCard && selectedInventoryCards.some(selectedCard => selectedCard.idcard === card.idcard) ? 'bg-green-600' : '', 
+                   removeCard && selectedInventoryCards.some(selectedCard => selectedCard.idcard === card.idcard) ? 'bg-red-600' : '' ]">
+                <div class="absolute top-2 left-2 bg-yellow-500 text-white text-sm px-2 py-1 rounded">
+                  ID: {{ card.idcard }}
                 </div>
-            </li>
-        </ul>
+                <div class="absolute bottom-2 w-full text-center text-sm text-yellow-300 font-semibold">
+                  {{ card.cardname }}
+                </div>
+              </div>
+            </div>
+            <button @click="editingDeck" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-4">Edited</button>
+            <p>Character: <span v-for="char in item.character" :key="char">(ID: {{ char }})</span></p>
+          </div>
+        </li>
+      </ul>
     </div>
-</template>
-
+  </template>
 <style scoped></style>
--- src/components/mainGameComponents/PlayerCharacter.vue
+++ src/components/mainGameComponents/PlayerCharacter.vue
@@ -7,12 +7,12 @@ const characters = ref(null);
 onMounted(async () => {
     const response = await fetch('data/db.json');
     const data = await response.json();
-    characters.value = data.character.find(cha => cha.id === props.selectId);
+    characters.value = data.character.find(cha => cha.idcharacter === props.selectId);
 })
 </script>
 
 <template>
-    <div v-if="characters" class="character-card">
+    <div v-if="characters" class="flex-col items-center text-center p-4 shadow-lg rounded-lg bg-white">
         <slot name="name">{{ characters.charatername }}</slot>
         <slot name="image">
             <img :src="`/images/${characters.charatername.toLowerCase()}.png`" :alt="characters.charatername">
